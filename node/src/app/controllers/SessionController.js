97974728961f3c66ff429128f51fe3fa8d2e2979
FelipeCararo <felipe.gorges.cararo@gmail.com>
2022-01-27 13:55:00 -0300

    feat: add workflows

--- node/src/app/controllers/SessionController.js
+++ node/src/app/controllers/SessionController.js
@@ -7,7 +7,7 @@ class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
-    console.log(user);
+
     if (!user) {
       return res.status(401).json({ message: 'User not found' });
     }
